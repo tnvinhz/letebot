8c0e0dd30feba97a5597519e7f3d3d3364b524b4
vinhtn <vinhtn@mobgame.vn>
2018-07-17 13:58:42 +0700

    simsimi test

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
-
+﻿
 
 var restify = require('restify');
 var builder = require('botbuilder');
-var azure = require('botbuilder-azure'); 
+var azure = require('botbuilder-azure');
+var Simsimi = require('simsimi');
+
+var simsimi = new Simsimi({
+    key: '6aebc819-b0ba-4cf3-bcb6-12d847bbdabd'
+});
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 8080, function () {
-   console.log('%s listening to %s', server.name, server.url);
+    console.log('%s listening to %s', server.name, server.url);
 });
 
 var documentDbOptions = {
-    host: 'https://letedb.documents.azure.com:443/', 
-    masterKey: 't21FPEcKBrrgVefyzmEh0teuJ9zdce6NSspCPsDGa3ROOOxJmAp7DViV5NnjRtU13m79Kuqev5QXnrChv1Eo0g==', 
-    database: 'botdocs',   
+    host: 'https://letedb.documents.azure.com:443/',
+    masterKey: 't21FPEcKBrrgVefyzmEh0teuJ9zdce6NSspCPsDGa3ROOOxJmAp7DViV5NnjRtU13m79Kuqev5QXnrChv1Eo0g==',
+    database: 'botdocs',
     collection: 'botdata'
 };
- 
+
 // Skypebot credentials
 var connector = new builder.ChatConnector({
     appId: 'ca632273-f04f-463d-b8ed-5c8509c1ca82',
@@ -26,24 +31,23 @@ var docDbClient = new azure.DocumentDbClient(documentDbOptions);
 
 var cosmosStorage = new azure.AzureBotStorage({ gzipData: false }, docDbClient);
 
-var bot = new builder.UniversalBot(connector);
+var bot = new builder.UniversalBot(connector).set('storage', cosmosStorage);
 server.post('/api/messages', connector.listen());
- 
-bot.set('storage', cosmosStorage);
+
 bot.on('contactRelationUpdate', function (message) {
     if (message.action === 'add') {
         var name = message.user ? message.user.name : null;
         var reply = new builder.Message()
-                .address(message.address)
-                .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
+            .address(message.address)
+            .text("Hello %s... Thanks for adding me. Say 'hello' to see some great demos.", name || 'there');
         bot.send(reply);
     } else {
         // delete their data
     }
 });
- 
+
 bot.on('typing', function (message) {
-  // User is typing
+    // User is typing
 });
 bot.on('deleteUserData', function (message) {
     // User asked to delete their data
@@ -51,15 +55,24 @@ bot.on('deleteUserData', function (message) {
 //=========================================================
 // Bots Dialogs
 //=========================================================
-String.prototype.contains = function(content){
-  return this.indexOf(content) !== -1;
-}
+String.prototype.contains = function (content) {
+    return this.indexOf(content) !== -1;
+};
 bot.dialog('/', function (session) {
-    if(session.message.text.toLowerCase().contains('hello')){
-      session.send('Hey, How are you?');
-      }else if(session.message.text.toLowerCase().contains('help')){
+    simsimi.listen(session.message.text, function (err, msg) {
+        if (err) return console.error(err);
+        session.send(msg);
+    });
+    if (session.message.text.toLowerCase().contains('chào bot')) {
+        session.send(' hello, em là letebot, đệ của a Vinh ạ');
+    } else if (session.message.text.toLowerCase().contains('help')) {
         session.send('How can I help you?');
-      }else{
-        session.send("Sorry I don't understand you...");
-      }
+    } else if (session.message.text.toLowerCase().contains(('chào chị Thúy đi e').toLowerCase())) {
+        session.send('em chào chị Thúy xinh đẹp ạ');
+    } else {
+        simsimi.listen(session.message.text, function (err, msg) {
+            if (err) return console.error(err);
+            session.send(msg);
+        });
+    }
 });
